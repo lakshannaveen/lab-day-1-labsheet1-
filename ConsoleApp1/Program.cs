ab3bce55d6e59387bef647004ba3b4bc0194ddca
User <User@ASUS>
2024-01-24 15:06:42 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter your width:");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter your length:");
+            double length = double.Parse(Console.ReadLine());
+
+            double area = Area(length ,width);
+
+            Console.Write("Your area is :" + area);
+            Console.ReadLine();
+
+            
+
+
+        }
+        static double Area(double length,double width)
+        {
+            return length * width;
+        }
+    }
+}
---
+++ q2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("\nEnter a numbers");
+                 number = int.Parse(Console.ReadLine());
+                if(number %2 == 0)
+                {
+                    Console.Write("It is even");
+                }
+                else
+                {
+                    Console.Write("It is odd");
+                }
+            }
+            Console.ReadLine();
+            }
+
+
+    }
+}
